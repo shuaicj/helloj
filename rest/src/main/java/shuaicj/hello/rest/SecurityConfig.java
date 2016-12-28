186b7b862e62e30badc1831aedb3d1605bcc9a6a
shuaicj <shuaicj@gmail.com>
2016-12-28 16:38:11 +0800

    add spring-security http basic authentication

---
+++ rest/src/main/java/shuaicj/hello/rest/SecurityConfig.java
@@ -0,0 +1,32 @@
+package shuaicj.hello.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Description.
+ *
+ * @author shuaicj 2016/12/28
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("shuaicj").password("shuaicj").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/hello").permitAll()
+//                .anyRequest().hasRole("USER")
+//                .and()
+//            .httpBasic();
+//        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    }
+}
+
