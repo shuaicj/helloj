f5378f13793570182a485a7e63b44ee743face63
shuaicj <shuaicj@gmail.com>
2016-12-29 10:04:57 +0800

    test GetMapping

--- hello-rest/src/main/java/shuaicj/hello/rest/HelloController.java
+++ hello-rest/src/main/java/shuaicj/hello/rest/HelloController.java
@@ -1,6 +1,6 @@
 package shuaicj.hello.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class HelloController {
     private static final String template = "Hello %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = "/hello")
+    @GetMapping("/hello")
     public Hello hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Hello(counter.incrementAndGet(), String.format(template, name));
     }
