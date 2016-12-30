190057360103b850ab7c0d50ed7e9be6bf7d5ed1
shuaicj <shuaicj@gmail.com>
2016-12-30 23:46:47 +0800

    add HelloMath.sqrt()

--- hello-math/src/main/java/shuaicj/hello/math/HelloMath.java
+++ hello-math/src/main/java/shuaicj/hello/math/HelloMath.java
@@ -14,4 +14,22 @@ public class HelloMath {
     public int sub(int a, int b) {
         return a - b;
     }
+
+    public double sqrt(double v, double offset) throws IllegalArgumentException {
+        if (v <= 1 || offset <= 0 || offset >= 1) {
+            throw new IllegalArgumentException();
+        }
+        double start = 0, end = v, mid = 0;
+        while (true) {
+            mid = (start + end) / 2;
+            if ((mid - offset) * (mid - offset) < v && (mid + offset) * (mid + offset) > v) {
+                return mid;
+            } else if (mid * mid < v) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+    }
+
 }
--- hello-math/src/test/java/shuaicj/hello/math/HelloMathTest.java
+++ hello-math/src/test/java/shuaicj/hello/math/HelloMathTest.java
@@ -3,6 +3,8 @@ package shuaicj.hello.math;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.within;
 
 /**
  * Description.
@@ -23,6 +25,15 @@ public class HelloMathTest {
     public void sub() throws Exception {
         assertThat(math.sub(2, 1)).isEqualTo(1);
         assertThat(math.sub(3, 1)).isEqualTo(2);
+    }
+
+    @Test
+    public void sqrt() throws Exception {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> math.sqrt(0.5, 0.1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> math.sqrt(9, -0.1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> math.sqrt(9, 2));
+        assertThat(math.sqrt(3*3, 0.1)).isCloseTo(3, within(0.1));
+        assertThat(math.sqrt(19*19, 0.5)).isCloseTo(19, within(0.5));
     }
 
 }
