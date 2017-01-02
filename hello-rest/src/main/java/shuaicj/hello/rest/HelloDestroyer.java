57d18dab9cc25e5a96bbc3256c1c5ff770eed201
shuaicj <shuaicj@gmail.com>
2017-01-02 16:12:13 +0800

    PreDestroy test

---
+++ hello-rest/src/main/java/shuaicj/hello/rest/HelloDestroyer.java
@@ -0,0 +1,23 @@
+package shuaicj.hello.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * Description.
+ *
+ * @author shuaicj 2017/01/02
+ */
+@Component
+public class HelloDestroyer {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloDestroyer.class);
+
+    @PreDestroy
+    public void destroy() {
+        logger.info("This is called @PreDestroy!");
+    }
+}
