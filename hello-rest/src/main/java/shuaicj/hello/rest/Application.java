b1904259f49f335107405e5111346923db80e4ce
shuaicj <shuaicj@gmail.com>
2017-01-02 16:12:58 +0800

    spring boot packaging war

--- hello-rest/src/main/java/shuaicj/hello/rest/Application.java
+++ hello-rest/src/main/java/shuaicj/hello/rest/Application.java
@@ -2,6 +2,8 @@ package shuaicj.hello.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Description.
@@ -9,7 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author shuaicj 2016/12/28
  */
 @SpringBootApplication
-public class Application {
+public class Application  extends SpringBootServletInitializer {
+
+    // for spring boot packaging: war
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
 
