5f208f41011bc40d8e9cceea62f57e2afba98a41
shuaicj <shuaicj@gmail.com>
2017-01-10 17:25:38 +0800

    maven main class config

---
+++ hello-maven-main/src/main/java/shuaicj/hello/maven/Application.java
@@ -0,0 +1,13 @@
+package shuaicj.hello.maven;
+
+/**
+ * Test configration of maven main class.
+ *
+ * @author shuaicj 2017/01/10
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("It's in main.");
+    }
+}
