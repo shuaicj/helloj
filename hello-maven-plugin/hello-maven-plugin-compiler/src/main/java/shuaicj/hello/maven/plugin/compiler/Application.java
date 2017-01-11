697b32a5f58dc352481785d65d38e991593d9d59
shuaicj <shuaicj@gmail.com>
2017-01-11 10:48:02 +0800

    maven plugin comiler

---
+++ hello-maven-plugin/hello-maven-plugin-compiler/src/main/java/shuaicj/hello/maven/plugin/compiler/Application.java
@@ -0,0 +1,14 @@
+package shuaicj.hello.maven.plugin.compiler;
+
+/**
+ * Test maven-compiler-plugin.
+ *
+ * @author shuaicj 2017/01/11
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("This is in " + Application.class.getName());
+    }
+}
+
