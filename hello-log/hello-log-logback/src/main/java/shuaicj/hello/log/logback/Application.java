c7eb29a17addb4ae8d248ee7f487458e9faefc88
shuaicj <shuaicj@gmail.com>
2017-03-21 16:37:55 +0800

    hello-log-logback: more detailed usage

--- hello-log/hello-log-logback/src/main/java/shuaicj/hello/log/logback/Application.java
+++ hello-log/hello-log-logback/src/main/java/shuaicj/hello/log/logback/Application.java
@@ -2,6 +2,8 @@ package shuaicj.hello.log.logback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,7 +20,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        logger.info("info in Application");
-        logger.debug("debug in Application");
+        Marker marker = MarkerFactory.getMarker("tag");
+
+        logger.info(marker, "info in Application");
+        logger.debug(marker, "debug in Application");
+        // for (int i = 0; i < 100000; i++) {
+        //     logger.info("shuaicj.log.bysize {}", i);
+        // }
     }
 }
