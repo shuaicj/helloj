1fe11fdeb4e0c74aec5f2a682f4a63b383e537fb
shuaicj <shuaicj@gmail.com>
2017-04-01 18:24:13 +0800

    hello-persist-mongo: array operations

--- hello-persist/hello-persist-mongo/src/main/java/shuaicj/hello/persist/mongo/User.java
+++ hello-persist/hello-persist-mongo/src/main/java/shuaicj/hello/persist/mongo/User.java
@@ -1,5 +1,10 @@
 package shuaicj.hello.persist.mongo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -7,85 +12,35 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * A java bean representing a user.
  *
  * @author shuaicj 2017/03/04
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Document(collection = "user")
 public class User {
 
     @Id
     private String id;
 
+    @NonNull
     @Indexed(unique = true)
     private String username;
 
+    @NonNull
     private String password;
+
+    private List<String> phones;
 
     @CreatedDate
     private Date createdTime;
 
     @LastModifiedDate
     private Date updatedTime;
-
-    public User() {
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public Date getUpdatedTime() {
-        return updatedTime;
-    }
-
-    public void setUpdatedTime(Date updatedTime) {
-        this.updatedTime = updatedTime;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", createdTime=" + createdTime +
-                ", updatedTime=" + updatedTime +
-                '}';
-    }
 }
--- hello-persist/hello-persist-mongo/src/test/java/shuaicj/hello/persist/mongo/UserRepositoryTest.java
+++ hello-persist/hello-persist-mongo/src/test/java/shuaicj/hello/persist/mongo/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package shuaicj.hello.persist.mongo;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,30 +27,40 @@ public class UserRepositoryTest {
 
     private static final String NAME = "shuaicj";
     private static final String PASS = "pass123";
+    private static final String PHONE1 = "phone1";
+    private static final String PHONE2 = "phone2";
 
     @Autowired
     private UserRepository repo;
 
     @Before
     public void setUp() throws Exception {
-        repo.deleteByUsername(NAME);
-        repo.save(new User(NAME, PASS));
+        repo.deleteAll();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        setUp();
     }
 
     @Test
     public void delete() throws Exception {
+        repo.save(new User(NAME, PASS));
+        assertThat(repo.findByUsername(NAME)).isNotNull();
         repo.deleteByUsername(NAME);
         assertThat(repo.findByUsername(NAME)).isNull();
     }
 
     @Test
     public void save() throws Exception {
-        repo.deleteByUsername(NAME);
-        repo.save(new User(NAME, PASS));
-        User u = repo.findByUsername(NAME);
+        User u = new User(NAME, PASS);
+        u.setPhones(Arrays.asList(PHONE1, PHONE2));
+        repo.save(u);
+        u = repo.findByUsername(NAME);
         assertThat(u).isNotNull();
         assertThat(u.getUsername()).isEqualTo(NAME);
         assertThat(u.getPassword()).isEqualTo(PASS);
+        assertThat(u.getPhones()).containsExactly(PHONE1, PHONE2);
         assertThat(u.getCreatedTime()).isNotNull();
         assertThat(u.getUpdatedTime()).isEqualTo(u.getCreatedTime());
     }
@@ -54,10 +68,12 @@ public class UserRepositoryTest {
     @Test(expected = DuplicateKeyException.class)
     public void duplicate() throws Exception {
         repo.save(new User(NAME, PASS));
+        repo.save(new User(NAME, PASS));
     }
 
     @Test
-    public void update() throws Exception {
+    public void updateTime() throws Exception {
+        repo.save(new User(NAME, PASS));
         User u = repo.findByUsername(NAME);
         assertThat(u.getCreatedTime()).isEqualTo(u.getUpdatedTime());
 
@@ -70,5 +86,22 @@ public class UserRepositoryTest {
         assertThat(u2.getPassword()).isEqualTo("newpass");
         assertThat(u2.getCreatedTime()).isEqualTo(u.getCreatedTime());
         assertThat(u2.getUpdatedTime()).isAfter(u.getCreatedTime());
+    }
+
+    @Test
+    public void arrayOps() throws Exception {
+        User u = new User(NAME, PASS);
+        u.setPhones(new ArrayList<>(Arrays.asList(PHONE1, PHONE2)));
+        u = repo.save(u);
+        assertThat(u.getPhones()).containsExactly(PHONE1, PHONE2);
+        u.getPhones().add(PHONE1);
+        u = repo.save(u);
+        assertThat(u.getPhones()).containsExactly(PHONE1, PHONE2, PHONE1);
+        u.getPhones().remove(0);
+        u = repo.save(u);
+        assertThat(u.getPhones()).containsExactly(PHONE2, PHONE1);
+        u.getPhones().set(1, PHONE2);
+        u = repo.save(u);
+        assertThat(u.getPhones()).containsExactly(PHONE2, PHONE2);
     }
 }
