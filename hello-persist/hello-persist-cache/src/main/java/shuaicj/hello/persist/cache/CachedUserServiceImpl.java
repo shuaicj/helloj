ddeef52b0abacb2d6ae6b178e3013a87d0235565
shuaicj <shuaicj@gmail.com>
2017-04-19 16:38:56 +0800

    hello-persist-cache: move cache annotations to interface

--- hello-persist/hello-persist-cache/src/main/java/shuaicj/hello/persist/cache/CachedUserServiceImpl.java
+++ hello-persist/hello-persist-cache/src/main/java/shuaicj/hello/persist/cache/CachedUserServiceImpl.java
@@ -1,9 +1,5 @@
 package shuaicj.hello.persist.cache;
 
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,22 +8,18 @@ import org.springframework.stereotype.Service;
  * @author shuaicj 2017/02/13
  */
 @Service
-@CacheConfig(cacheNames = "userCache")
 public class CachedUserServiceImpl implements CachedUserService {
 
     @Override
-    @Cacheable
     public User find(String username) {
         return new User(username, "china");
     }
 
     @Override
-    @CacheEvict
     public void clearCache(String username) {
     }
 
     @Override
-    @CachePut(key = "#username")
     public User update(String username, String address) {
         return new User(username, address);
     }
