30f5e6241dd2b8308bd6a1813a81aba92574cee8
shuaicj <shuaicj@gmail.com>
2019-02-20 22:08:54 +0800

    localhost hostname

--- hello-ip-hostname/src/main/java/shuaicj/hello/ip/hostname/Application.java
+++ hello-ip-hostname/src/main/java/shuaicj/hello/ip/hostname/Application.java
@@ -5,18 +5,27 @@ import java.net.NetworkInterface;
 import java.util.Collections;
 
 /**
- * Get all IPs and hostnames of current machine.
+ * Get all IPs and host names of current machine.
  *
- * @author shuaicj 2018/10/11
+ * @author shuaicj 2019/02/20
  */
 public class Application {
 
     public static void main(String[] args) throws Exception {
+        System.out.println("-------------- localhost --------------");
+        InetAddress localhost = InetAddress.getLocalHost();
+        System.out.println("hostname   : " + localhost.getHostName());
+        System.out.println("fqdn       : " + localhost.getCanonicalHostName());
+        System.out.println("ip address : " + localhost.getHostAddress());
+        System.out.println();
+
         for (NetworkInterface ni : Collections.list(NetworkInterface.getNetworkInterfaces())) {
             System.out.println("-------------- " + ni.getName() + " --------------");
             for (InetAddress ia : Collections.list(ni.getInetAddresses())) {
-                System.out.println("hostname: " + ia.getHostName());
-                System.out.println("ip: " + ia.getHostAddress());
+                System.out.println("hostname   : " + ia.getHostName());
+                System.out.println("fqdn       : " + ia.getCanonicalHostName());
+                System.out.println("ip address : " + ia.getHostAddress());
+                System.out.println();
             }
         }
     }
